5e96803a44aa40e51f31d0e41dd084ab6b918369
unknown <Jeanine Vincent@MSI>
2020-07-04 07:53:34 -0500

    PyPoll completed

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,74 +4,76 @@ import os
 # Module for reading CSV files
 import csv
 
-# Create variables
-total_months = 86
-total_dollars = 38382578
-avg_change = -2315.12
-greatest_increase_profit = 1926159
-greatest_increase_date = "Feb-2012"
-greatest_decrease_profit = -2196167
-greatest_decrease_date = "Sep-2013"
+# Declare analysis variables
+total_votes = 0
+candidates = {}
+winner_vote_count = 0
+winner = ""
 
 
-# Input csv file
-csvpath = os.path.join('', 'Resources', 'budget_data.csv')
+# Path to input data
+csvpath = os.path.join('', 'Resources', 'election_data.csv')
 
 # Open the CSV
-with open(csvpath) as csvfile:
+with open(csvpath, 'r') as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print('********** csvreader ******************')
-    print(csvreader)
-
-    # Read the header row first
+    # print("print csvreader.....")
+    
+    # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
-    print('********** CVS Header ******************')
-    print(f"CSV Header: {csv_header}")
+    # print(f"CSV Header: {csv_header}")
 
-    # Read each row of data after the header
     for row in csvreader:
-        print(row)
+        profit_loss = row[1]
 
+        total_votes = total_votes + 1
+        name = row[2]
+
+        if name not in candidates:
+            candidates[name] = 1
+        else:
+            candidates[name] = candidates[name] + 1
 
 # Specify the file to write to
-output_path = os.path.join("", "Analysis", "financial.txt")
+output_path = os.path.join("", "Analysis", "election.txt")
 
 # Open the output file, create a header row, and then write the data contents to the csv
 with open(output_path, 'w') as txt_file:
 
     # Write the output 
-    txt_file.write('Financial Analysis')
+    txt_file.write('Election Results')
     txt_file.write('\n')
     txt_file.write('----------------------------')
     txt_file.write('\n')
-    txt_file.write(f'Total Months: {total_months}')
+    txt_file.write(f'Total Votes: {total_votes}')
     txt_file.write('\n')
-    txt_file.write(f'Total: ${total_dollars}')
+    txt_file.write('----------------------------')
     txt_file.write('\n')
-    txt_file.write(f'Average Change: ${avg_change}')
-    txt_file.write('\n')
-    txt_file.write(f'Greatest Increase in Profits: {greatest_increase_date}  (${greatest_increase_profit})')
-    txt_file.write('\n')
-    txt_file.write(f'Greatest Decrease in Profits: {greatest_decrease_date}  (${greatest_decrease_profit})')
 
-   
-# Print to terminal
-    print('********** Print to terminal ******************')
-    print('Financial Analysis')
-    print('----------------------------')
-    print(f'Total Months: {total_months}')
-    print(f'Total: {total_dollars}')
-    print(f'Average Change: {avg_change}')
-    print(f'Greatest Increase in Profits: {greatest_increase_date}  {greatest_increase_profit}')
-    print(f'Greatest Decrease in Profits: {greatest_decrease_date}  {greatest_decrease_profit}')
+    # print(candidates)
 
-    
+    for candidate_name, vote_count in candidates.items():
+        percentage = '{0:.3%}'.format((vote_count / total_votes))
+        vote_count = (vote_count)
+        
+        txt_file.write(f"{candidate_name}: {percentage} ({(vote_count)})")
+        txt_file.write('\n')
+
+        if vote_count > winner_vote_count:
+            winner_vote_count = vote_count
+            winner = candidate_name
+
+    txt_file.write('----------------------------')
+    txt_file.write('\n')
+    txt_file.write(f'Winner: {winner}')
+    txt_file.write('\n')
+    txt_file.write('----------------------------')
+
 
 # Open the output file in "read" mode ('r') and print to terminal
-print('********** Print from the output file ******************')
 with open(output_path, 'r') as read_txt:
     print(read_txt.read())
 
