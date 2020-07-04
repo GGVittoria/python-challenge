dbf12f7d75305c5792f75f104931348cdb634536
unknown <Jeanine Vincent@MSI>
2020-07-04 07:00:06 -0500

    PyBank completed

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,22 +4,98 @@ import os
 # Module for reading CSV files
 import csv
 
+# Declare analysis variables
+total_months = 0
+previous_amt = 0
+total_dollars = 0
+first_value = True
+
+
+# Path to input data
 csvpath = os.path.join('', 'Resources', 'budget_data.csv')
 
-
-
 # Open the CSV
-with open(csvpath) as csvfile:
+with open(csvpath, 'r') as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
-
+    # print("print csvreader.....")
+    
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    # print(f"CSV Header: {csv_header}")
 
-    # Read each row of data after the header
     for row in csvreader:
-        print(row)
+        date = row[0]
+        profit_loss = row[1]
+
+        total_months = total_months + 1
+
+        total_dollars = total_dollars + int(profit_loss)
+
+        if first_value:
+            change_amt = 0
+            total_change = 0
+            change_count = 0
+            first_value = False
+            greatest_increase_profit = 0
+            greatest_increase_date = ""
+            greatest_decrease_profit = 0
+            greatest_decrease_date = ""
+        else:
+            change_amt = int(profit_loss) - previous_amt
+
+            total_change = total_change + change_amt
+
+            change_count = change_count + 1
+
+            if change_amt > greatest_increase_profit:
+                greatest_increase_profit = change_amt
+                greatest_increase_date = date
+            if change_amt < greatest_decrease_profit:
+                greatest_decrease_profit = change_amt
+                greatest_decrease_date = date
+
+        previous_amt = int(profit_loss)
+
+    average_change = "{0:.2f}".format(total_change / change_count)
+
+ 
+# Specify the file to write to
+output_path = os.path.join("", "Analysis", "financial.txt")
+
+# Open the output file, create a header row, and then write the data contents to the csv
+with open(output_path, 'w') as txt_file:
+
+    # Write the output 
+    txt_file.write('Financial Analysis')
+    txt_file.write('\n')
+    txt_file.write('----------------------------')
+    txt_file.write('\n')
+    txt_file.write(f'Total Months: {total_months}')
+    txt_file.write('\n')
+    txt_file.write(f'Total: ${total_dollars}')
+    txt_file.write('\n')
+    txt_file.write(f'Average Change: ${average_change}')
+    txt_file.write('\n')
+    txt_file.write(f'Greatest Increase in Profits: {greatest_increase_date}  (${greatest_increase_profit})')
+    txt_file.write('\n')
+    txt_file.write(f'Greatest Decrease in Profits: {greatest_decrease_date}  (${greatest_decrease_profit})')
+
+   
+# Open the output file in "read" mode ('r') and print to terminal
+with open(output_path, 'r') as read_txt:
+    print(read_txt.read())
+
+
+txt_file.close()
+
+# # Print to terminal
+#     print('Financial Analysis')
+#     print('----------------------------')
+#     print(f'Total Months: {total_months}')
+#     print(f'Total: {total_dollars}')
+#     print(f'Average Change: {average_change}')
+#     print(f'Greatest Increase in Profits: {greatest_increase_date}  {greatest_increase_profit}')
+#     print(f'Greatest Decrease in Profits: {greatest_decrease_date}  {greatest_decrease_profit}')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,9 +4,18 @@ import os
 # Module for reading CSV files
 import csv
 
-csvpath = os.path.join('', 'Resources', 'election_data.csv')
+# Create variables
+total_months = 86
+total_dollars = 38382578
+avg_change = -2315.12
+greatest_increase_profit = 1926159
+greatest_increase_date = "Feb-2012"
+greatest_decrease_profit = -2196167
+greatest_decrease_date = "Sep-2013"
 
 
+# Input csv file
+csvpath = os.path.join('', 'Resources', 'budget_data.csv')
 
 # Open the CSV
 with open(csvpath) as csvfile:
@@ -14,12 +23,57 @@ with open(csvpath) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
+    print('********** csvreader ******************')
     print(csvreader)
 
-    # Read the header row first (skip this step if there is now header)
+    # Read the header row first
     csv_header = next(csvreader)
+    print('********** CVS Header ******************')
     print(f"CSV Header: {csv_header}")
 
     # Read each row of data after the header
     for row in csvreader:
         print(row)
+
+
+# Specify the file to write to
+output_path = os.path.join("", "Analysis", "financial.txt")
+
+# Open the output file, create a header row, and then write the data contents to the csv
+with open(output_path, 'w') as txt_file:
+
+    # Write the output 
+    txt_file.write('Financial Analysis')
+    txt_file.write('\n')
+    txt_file.write('----------------------------')
+    txt_file.write('\n')
+    txt_file.write(f'Total Months: {total_months}')
+    txt_file.write('\n')
+    txt_file.write(f'Total: ${total_dollars}')
+    txt_file.write('\n')
+    txt_file.write(f'Average Change: ${avg_change}')
+    txt_file.write('\n')
+    txt_file.write(f'Greatest Increase in Profits: {greatest_increase_date}  (${greatest_increase_profit})')
+    txt_file.write('\n')
+    txt_file.write(f'Greatest Decrease in Profits: {greatest_decrease_date}  (${greatest_decrease_profit})')
+
+   
+# Print to terminal
+    print('********** Print to terminal ******************')
+    print('Financial Analysis')
+    print('----------------------------')
+    print(f'Total Months: {total_months}')
+    print(f'Total: {total_dollars}')
+    print(f'Average Change: {avg_change}')
+    print(f'Greatest Increase in Profits: {greatest_increase_date}  {greatest_increase_profit}')
+    print(f'Greatest Decrease in Profits: {greatest_decrease_date}  {greatest_decrease_profit}')
+
+    
+
+# Open the output file in "read" mode ('r') and print to terminal
+print('********** Print from the output file ******************')
+with open(output_path, 'r') as read_txt:
+    print(read_txt.read())
+
+
+txt_file.close()
